13e0c0d93975b0a7f36eb32e0406f4dbdf32677d
Ricardo Otero <oterosantos@gmail.com>
2014-02-03 19:31:22 +0000

    New class for fetching personal information

---
+++ lib/infostrada/person_info.rb
@@ -0,0 +1,28 @@
+module Infostrada
+  class PersonInfo < Infostrada::BaseRequest
+    URL = '/GetPersonInfo'
+
+    attr_accessor :id, :first_name, :last_name, :public_name, :nickname, :height, :birthdate
+    attr_accessor :weight, :birth_city, :birth_country
+
+    def self.where(options)
+      id = options.delete(:person_id)
+      info = get(URL, query: { personid: id }).first
+      self.new(id, info)
+    end
+
+    def initialize(id, info)
+      @id = id
+
+      @first_name = info['c_FirstName']
+      @last_name = info['c_LastName']
+      @public_name = info['c_PublicName']
+      @nickname = info['c_Nickname']
+      @birthdate = Formatter.format_date(info['d_BirthDate'])
+      @height = info['n_Height']
+      @weight = info['n_Weight']
+      @birth_city = info['c_BirthCity']
+      @birth_country = info['c_BirthCountry']
+    end
+  end
+end
