bb81e211930676cd2954b1c0e513710e71695569
Ricardo Otero <oterosantos@gmail.com>
2014-02-03 19:32:36 +0000

    Only one URL used, so no need for Hash.

--- lib/infostrada/squad.rb
+++ lib/infostrada/squad.rb
@@ -1,14 +1,12 @@
 module Infostrada
   class Squad < Infostrada::BaseRequest
-    URLS = {
-      get: '/GetSquad'
-    }
+    URL = '/GetSquad'
 
     def self.where(options = {})
       edition_id = options.delete(:edition_id)
       team_id = options.delete(:team_id)
 
-      list = get(URLS[:get], query: { editionid: edition_id, teamid: team_id })
+      list = get(URL, query: { editionid: edition_id, teamid: team_id })
 
       players = []
       list.each do |player_hash|
