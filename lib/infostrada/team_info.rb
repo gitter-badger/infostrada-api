51007af46ddb1b8ab9340a71df7541e15fc60d71
Ricardo Otero <oterosantos@gmail.com>
2014-04-07 18:38:40 +0100

    Added country information on TeamInfo

--- lib/infostrada/team_info.rb
+++ lib/infostrada/team_info.rb
@@ -4,7 +4,7 @@ module Infostrada
   class TeamInfo < Infostrada::BaseRequest
     attr_accessor :official_name, :official_short_name, :public_name, :public_short_name, :nickname
     attr_accessor :foundation_date, :official_stadium_name, :stadium_name, :stadium_capacity
-    attr_accessor :url, :city
+    attr_accessor :url, :city, :country, :country_short_name
 
     URL = '/GetTeamInfo'
 
@@ -15,17 +15,19 @@ module Infostrada
     end
 
     def initialize(hash)
-      @official_name          =  hash['c_OfficialName']
-      @official_short_name    =  hash['c_OfficialNameSort']
-      @public_name            =  hash['c_PublicName']
-      @public_short_name      =  hash['c_PublicNameSort']
-      @nickname               =  hash['c_Nickname']
-      @foundation_date        =  hash['d_FoundationDate']
-      @official_stadium_name  =  hash['c_StadiumOfficialName']
-      @stadium_name           =  hash['c_Stadium']
-      @stadium_capacity       =  hash['n_StadiumCapacity']
-      @url                    =  hash['c_URL']
-      @city                   =  hash['c_City']
+      @official_name          = hash['c_OfficialName']
+      @official_short_name    = hash['c_OfficialNameSort']
+      @public_name            = hash['c_PublicName']
+      @public_short_name      = hash['c_PublicNameSort']
+      @nickname               = hash['c_Nickname']
+      @foundation_date        = hash['d_FoundationDate']
+      @official_stadium_name  = hash['c_StadiumOfficialName']
+      @stadium_name           = hash['c_Stadium']
+      @stadium_capacity       = hash['n_StadiumCapacity']
+      @url                    = hash['c_URL']
+      @city                   = hash['c_City']
+      @country                = hash['c_Country']
+      @country_short_name     = hash['c_CountryShort']
 
       self
     end
