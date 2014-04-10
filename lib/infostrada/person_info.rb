6c03b896430556273a1fd82fe60ac086dd6b99b3
Ricardo Otero <oterosantos@gmail.com>
2014-04-10 18:48:57 +0100

    Rescue empty PersonInfo responses

--- lib/infostrada/person_info.rb
+++ lib/infostrada/person_info.rb
@@ -9,22 +9,27 @@ module Infostrada
 
     def self.where(options)
       id = options.delete(:person_id)
+
       info = get!(URL, query: { personid: id }).first
+
       self.new(id, info)
     end
 
     def initialize(id, info)
       @id = id
 
-      @first_name = info['c_FirstName']
-      @last_name = info['c_LastName']
-      @public_name = info['c_PublicName']
-      @nickname = info['c_Nickname']
-      @birthdate = Formatter.format_date(info['d_BirthDate'])
-      @height = info['n_Height']
-      @weight = info['n_Weight']
-      @birth_city = info['c_BirthCity']
-      @birth_country = info['c_BirthCountry']
+      # Sometimes info comes as an empty array...
+      if info && !info.empty?
+        @first_name = info['c_FirstName']
+        @last_name = info['c_LastName']
+        @public_name = info['c_PublicName']
+        @nickname = info['c_Nickname']
+        @birthdate = Formatter.format_date(info['d_BirthDate'])
+        @height = info['n_Height']
+        @weight = info['n_Weight']
+        @birth_city = info['c_BirthCity']
+        @birth_country = info['c_BirthCountry']
+      end
     end
   end
 end
