cc58c2eb33e2091505d3cae3ff38aae718f7796b
Ricardo Otero <oterosantos@gmail.com>
2014-04-22 11:51:55 +0100

    Added explanation

--- lib/infostrada/core_ext/string.rb
+++ lib/infostrada/core_ext/string.rb
@@ -1,4 +1,6 @@
 class String
+  # This is a version of the method stolen from railties and it converts 'Text-LikeThis' to
+  # 'text_like_this'.
   def snake_case
     result = self.gsub(/::/, '/')
     result.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
