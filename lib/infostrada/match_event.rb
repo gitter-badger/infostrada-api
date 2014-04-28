1c52e350cbad2574045569f9de25f8a20db04c2f
Ricardo Otero <oterosantos@gmail.com>
2014-04-28 19:54:26 +0100

    Added action_set to match event

--- lib/infostrada/match_event.rb
+++ lib/infostrada/match_event.rb
@@ -1,11 +1,33 @@
 module Infostrada
+  # MatchEvent can have a lot of different events in a match.
+  #
+  # Here's a list of the n_ActionSet values, from Infostrada FAQ:
+  #
+  # 1 = Goals
+  # 2 = Penalty shootout
+  # 3 = Cards
+  # 4 = Line-up
+  # 5 = Substitutions
+  # 6 = Substitutes not used
+  # 7 = Players not selected / suspended
+  # 8 = Coaches
+  # 9 = Officials
+  # 10 = Missed penalties
+  # 11 = Captain
+  # 12 = Free kick / Foul / Corner / Offside / Shot / Save
+  # 13 = Possession
+  # 14 = Temporarily out
+  # 15 = Other
+  # 16 = Time
   class MatchEvent
     attr_accessor :id, :description, :short_description, :period, :period_short, :time, :home_goals
     attr_accessor :away_goals, :home_event, :team_id, :team_name, :team_short_name, :person_id
     attr_accessor :person_name, :person_short_name, :reason, :info, :person2_id, :person2_name
-    attr_accessor :person2_short_name, :minute
+    attr_accessor :person2_short_name, :minute, :match_id, :action_set
 
-    def initialize(hash)
+    def initialize(match_id, hash)
+      @match_id = match_id
+
       @id                 = hash['n_ActionID']
       @description        = hash['c_Action']
       @short_description  = hash['c_ActionShort']
@@ -14,7 +36,7 @@ module Infostrada
       @time               = hash['n_ActionTime']
       @home_goals         = hash['n_HomeGoals']
       @away_goals         = hash['n_AwayGoals']
-      @home_event         = (hash['n_HomeOrAway'] == 1) ? true : false
+      @home_event         = hash['n_HomeOrAway'] == 1 ? true : false
       @team_id            = hash['n_TeamID']
       @team_name          = hash['c_Team']
       @team_short_name    = hash['c_TeamShort']
@@ -27,6 +49,7 @@ module Infostrada
       @person2_name       = hash['c_SubPerson']
       @person2_short_name = hash['c_SubPersonShort']
       @minute             = hash['c_ActionMinute']
+      @action_set         = hash['n_ActionSet']
 
       self
     end
